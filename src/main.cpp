d8089ef3f24e57ebe69a623ee38128f91cb65f1f
Nihar Turumella <nihar1912@live.com>
2020-09-27 19:50:52 -0500

    Works Great .. Just low speed.. try increasing desired speed
    Signed-off-by: Nihar Turumella <nihar1912@live.com>

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ int main() {
   controllerAngle.Init(0.3, 0.008, 3.5);
   
   PID controllerSpeed;
-  controllerSpeed.Init(0.15, 0.001, 1.2);
+  controllerSpeed.Init(0.1, 0.0, 1.5);
   double desiredSpeed = 35.0;
   
   bool optimiseAngle = false;
